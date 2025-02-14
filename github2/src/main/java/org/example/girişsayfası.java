e3f5d35df6cd3a8f0fb59a2e4727ac7636fd7a59
Halil İbrahim Köklü <halilibrahimm3446@gmail.com>
2025-02-14 18:39:43 +0300

    boş mu konturolü yapıldı

--- github2/src/main/java/org/example/girişsayfası.java
+++ github2/src/main/java/org/example/girişsayfası.java
@@ -2,11 +2,14 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class girişsayfası {
+public class girişsayfası implements ActionListener {
     JFrame frame;
     JTextField kullanıcıadı;
     JTextField şifre;
+    JButton button;
 
     public girişsayfası(){
         JPanel panelüst = new JPanel();
@@ -21,7 +24,7 @@ public class girişsayfası {
 
         JPanel panelalt = new JPanel();
         panelalt.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        JLabel labelalt = new JLabel("kullanıcı adı");
+        JLabel labelalt = new JLabel("şifre");
 
         şifre = new JTextField();
         şifre.setPreferredSize(new Dimension(200,40));
@@ -29,14 +32,31 @@ public class girişsayfası {
         panelalt.add(labelalt);
         panelalt.add(şifre);
 
+        button = new JButton();
+        button.addActionListener(this);
+
         frame = new JFrame("giriş sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,300);
         frame.setLayout(new GridLayout(3,1));
 
-
+        frame.add(button);
         frame.add(panelüst);
         frame.add(panelalt);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button){
+            String kullanıcıadı = this.kullanıcıadı .getText().trim();
+            String şifre = this.şifre.getText().trim();
+
+            if (kullanıcıadı.isEmpty() || şifre.isEmpty()){
+                JOptionPane.showMessageDialog(null,"lütfen tüm alanları doldur","hata",JOptionPane.ERROR_MESSAGE);
+
+            }
+
+        }
     }
 }
